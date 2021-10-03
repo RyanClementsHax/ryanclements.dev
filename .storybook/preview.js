d9df9421eda8943ff1cf48c3accfd778f69d5fce
Ryan Clements <ryanclementshax@gmail.com>
2021-10-03 16:44:42 -0400

    make tailwind work with storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { RouterContext } from 'next/dist/shared/lib/router-context'
+import * as NextImage from 'next/image'
+
+import 'styles/global.scss'
 
 export const parameters = {
   nextRouter: {
     Provider: RouterContext.Provider
   }
 }
-
-import * as NextImage from 'next/image'
 
 const OriginalNextImage = NextImage.default
 
