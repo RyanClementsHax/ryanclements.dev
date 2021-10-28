caa948b4a32d6b894a497c2137f6b0694aaf04f5
Ryan Clements <ryanclementshax@gmail.com>
2021-10-27 20:01:12 -0400

    refactor hero section to use tailwind container

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,26 +9,28 @@ export const Index: React.FC = () => {
         <title>Ryan Clements</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="h-screen grid md:grid-cols-2 lg:grid-cols-5 gap-4 content-center py-4 px-8 xl:px-16">
-        <div className="max-w-md justify-self-center grid content-center lg:col-span-2">
-          <h1 className="text-4xl font-bold mb-4">
-            Hiya 👋
-            <br />
-            I’m Ryan Clements
-          </h1>
-          <h2 className="text-2xl text-gray-600">
-            I 💖 God, my wife and daughter&nbsp;👨‍👩‍👧, and making dope
-            software&nbsp;👨‍💻
-          </h2>
-        </div>
-        <div className="relative h-[500px] hidden md:block overflow-hidden shadow-md lg:col-span-3">
-          <Image
-            src="/banner.jpg"
-            layout="fill"
-            alt="My wife, me, and our wedding party being silly"
-            objectFit="cover"
-            objectPosition="center"
-          />
+      <section className="h-screen w-100 p-8">
+        <div className="h-full grid gap-4 content-center md:container md:mx-auto md:grid-cols-2 lg:grid-cols-5">
+          <div className="grid content-center lg:col-span-2">
+            <h1 className="text-4xl font-bold mb-4">
+              Hiya 👋
+              <br />
+              I’m Ryan Clements
+            </h1>
+            <h2 className="text-2xl text-gray-600">
+              I 💖 God, my wife and daughter&nbsp;👨‍👩‍👧, and making dope
+              software&nbsp;👨‍💻
+            </h2>
+          </div>
+          <div className="relative h-[500px] hidden md:block shadow-md lg:col-span-3">
+            <Image
+              src="/banner.jpg"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+              alt="My wife, me, and our wedding party being silly"
+            />
+          </div>
         </div>
       </section>
     </Layout>
