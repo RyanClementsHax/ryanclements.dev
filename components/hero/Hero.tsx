1ec652c7967757a81091830018b7899708d49a39
Ryan Clements <ryanclementshax@gmail.com>
2021-11-01 20:34:13 -0400

    abstract hero section to its own component

---
+++ components/hero/Hero.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import banner from 'public/banner.jpg'
+
+export const Hero: React.FC = () => (
+  <section className="h-screen w-100 p-8">
+    <div className="h-full grid gap-4 content-center md:container md:mx-auto md:grid-cols-2 lg:grid-cols-5">
+      <div className="grid content-center lg:col-span-2">
+        <h1 className="text-4xl font-bold mb-4">
+          Hiya 👋
+          <br />
+          I’m Ryan Clements
+        </h1>
+        <h2 className="text-2xl text-gray-600">
+          I 💖 God, my wife and daughter&nbsp;👨‍👩‍👧, and making dope
+          software&nbsp;👨‍💻
+        </h2>
+      </div>
+      <div className="relative h-[500px] hidden md:block shadow-md lg:col-span-3">
+        <Image
+          src={banner}
+          layout="responsive"
+          objectFit="cover"
+          objectPosition="center"
+          placeholder="blur"
+          priority
+          alt="My wife, me, and our wedding party being silly"
+          className="h-full"
+        />
+      </div>
+    </div>
+  </section>
+)
