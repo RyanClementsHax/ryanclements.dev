711d2a600b611b52ef916a598d3c2713b6a25094
Ryan Clements <ryanclementshax@gmail.com>
2021-11-08 19:16:18 -0500

    fix css grid problems in hero section

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 import banner from 'public/banner.jpg'
 
 export const Hero: React.FC = () => (
-  <section className="h-screen w-100 p-8">
-    <div className="h-full grid gap-4 content-center md:container md:mx-auto md:grid-cols-2 lg:grid-cols-5">
-      <div className="grid content-center lg:col-span-2">
-        <h1 className="text-4xl font-bold mb-4 text-on-surface-base">
+  <section className="h-screen w-100 px-8 py-16">
+    <div className="h-full grid gap-4 items-center md:container md:mx-auto md:grid-cols-2 lg:grid-cols-5">
+      <div className="grid gap-4 lg:col-span-2">
+        <h1 className="text-4xl font-bold text-on-surface-base">
           Hiya 👋
           <br />
           I’m Ryan Clements
@@ -15,7 +15,7 @@ export const Hero: React.FC = () => (
           software&nbsp;👨‍💻
         </h2>
       </div>
-      <div className="relative h-[500px] hidden md:block overflow-hidden rounded-xl shadow-md lg:col-span-3">
+      <div className="relative h-full max-h-[500px] hidden md:block overflow-hidden rounded-xl shadow-md lg:col-span-3">
         <Image
           src={banner}
           layout="fill"
