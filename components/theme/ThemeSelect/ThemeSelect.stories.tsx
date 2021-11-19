82910dc8dfbf754bdd17e897a53a38b612817e07
Ryan Clements <ryanclementshax@gmail.com>
2021-11-19 09:58:21 -0500

    add figma urls for theme select stories

--- components/theme/ThemeSelect/ThemeSelect.stories.tsx
+++ components/theme/ThemeSelect/ThemeSelect.stories.tsx
@@ -6,7 +6,16 @@ import { ThemeSelect } from '.'
 const Template: Story<Parameters<typeof ThemeSelect>[0]> = props => (
   <ThemeSelect {...props} />
 )
-const { Base, DarkTheme } = createDefaultStories(Template)
+const { Base, DarkTheme } = createDefaultStories(Template, {
+  base: {
+    figmaUrl:
+      'https://www.figma.com/file/PdMqvzKJHKcSHXkdQ2SEp3/Personal-Website?node-id=1319%3A191'
+  },
+  darkTheme: {
+    figmaUrl:
+      'https://www.figma.com/file/PdMqvzKJHKcSHXkdQ2SEp3/Personal-Website?node-id=1319%3A197'
+  }
+})
 
 export { Base, DarkTheme }
 
