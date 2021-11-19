78645eda412860e93b6a6a6dfbf14462bdfb78d3
Ryan Clements <ryanclementshax@gmail.com>
2021-11-19 08:07:43 -0500

    add figma urls to hero stories

--- components/pages/landing/Hero/Hero.stories.tsx
+++ components/pages/landing/Hero/Hero.stories.tsx
@@ -5,8 +5,27 @@ import { Hero } from '.'
 
 const Template: Story<Parameters<typeof Hero>[0]> = props => <Hero {...props} />
 
-const { Base, Mobile, DarkTheme, DarkThemedMobile } =
-  createDefaultStories(Template)
+const { Base, Mobile, DarkTheme, DarkThemedMobile } = createDefaultStories(
+  Template,
+  {
+    base: {
+      figmaUrl:
+        'https://www.figma.com/file/PdMqvzKJHKcSHXkdQ2SEp3/Personal-Website?node-id=1312%3A38'
+    },
+    mobile: {
+      figmaUrl:
+        'https://www.figma.com/file/PdMqvzKJHKcSHXkdQ2SEp3/Personal-Website?node-id=1312%3A47'
+    },
+    darkTheme: {
+      figmaUrl:
+        'https://www.figma.com/file/PdMqvzKJHKcSHXkdQ2SEp3/Personal-Website?node-id=1312%3A55'
+    },
+    darkThemedMobile: {
+      figmaUrl:
+        'https://www.figma.com/file/PdMqvzKJHKcSHXkdQ2SEp3/Personal-Website?node-id=1312%3A64'
+    }
+  }
+)
 
 export { Base, Mobile, DarkTheme, DarkThemedMobile }
 
