2d38d267c4cef89e1cd19fbd1e926dfe2c208572
Ryan Clements <ryanclementshax@gmail.com>
2021-11-19 10:53:54 -0500

    add figma urls for header stories

--- components/header/Header.stories.tsx
+++ components/header/Header.stories.tsx
@@ -7,8 +7,27 @@ const Template: Story<Parameters<typeof Header>[0]> = props => (
   <Header {...props} />
 )
 
-const { Base, Mobile, DarkTheme, DarkThemedMobile } =
-  createDefaultStories(Template)
+const { Base, Mobile, DarkTheme, DarkThemedMobile } = createDefaultStories(
+  Template,
+  {
+    base: {
+      figmaUrl:
+        'https://www.figma.com/file/PdMqvzKJHKcSHXkdQ2SEp3/Personal-Website?node-id=1319%3A102'
+    },
+    mobile: {
+      figmaUrl:
+        'https://www.figma.com/file/PdMqvzKJHKcSHXkdQ2SEp3/Personal-Website?node-id=1319%3A100'
+    },
+    darkTheme: {
+      figmaUrl:
+        'https://www.figma.com/file/PdMqvzKJHKcSHXkdQ2SEp3/Personal-Website?node-id=1329%3A144'
+    },
+    darkThemedMobile: {
+      figmaUrl:
+        'https://www.figma.com/file/PdMqvzKJHKcSHXkdQ2SEp3/Personal-Website?node-id=1329%3A142'
+    }
+  }
+)
 
 export { Base, Mobile, DarkTheme, DarkThemedMobile }
 
