644c7a65d02bc744ba5eb239f2ab551d795d1198
Ryan Clements <ryanclementshax@gmail.com>
2022-12-22 20:26:20 -0500

    format date and add author info component in case I wanna use it

---
+++ components/pages/posts/[slug]/AuthorInfo/AuthorInfo.tsx
@@ -0,0 +1,40 @@
+import { A11yStaticImageData, SocialAccount } from 'lib/content'
+import { Author } from 'lib/content/authors'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export interface AuthorInfoProps {
+  author: Author
+}
+
+export const AuthorInfo: React.FC<AuthorInfoProps> = ({ author }) => (
+  <AuthorInfoContainer>
+    <Avatar src={{ ...author.avatar, alt: author.name }} />
+    <div className="flex flex-col gap-1">
+      <p>{author.name}</p>
+      <SocialLinks socialAccounts={author.socialAccounts} />
+    </div>
+  </AuthorInfoContainer>
+)
+
+const AuthorInfoContainer: React.FC<{ children?: React.ReactNode }> = ({
+  children
+}) => <div className="flex items-center gap-4">{children}</div>
+
+const Avatar: React.FC<{ src: A11yStaticImageData }> = ({
+  src: { alt, ...src }
+}) => (
+  <Image className="rounded-full" height={64} width={64} src={src} alt={alt} />
+)
+
+const SocialLinks: React.FC<{ socialAccounts: SocialAccount[] }> = ({
+  socialAccounts
+}) => (
+  <div className="flex flex-wrap gap-x-4">
+    {socialAccounts.map(x => (
+      <Link key={x.href} href={x.href} target="_blank">
+        {x.platform}
+      </Link>
+    ))}
+  </div>
+)
--- components/pages/posts/[slug]/MetaCard/MetaCard.tsx
+++ components/pages/posts/[slug]/MetaCard/MetaCard.tsx
@@ -1,3 +1,5 @@
+import { format } from 'date-fns'
+
 export interface MetaCardProps {
   title: string
   publishedOn: Date
@@ -6,7 +8,7 @@ export interface MetaCardProps {
 export const MetaCard: React.FC<MetaCardProps> = ({ title, publishedOn }) => (
   <MetaContainer>
     <Title>{title}</Title>
-    <PublishedDate>{publishedOn.toDateString()}</PublishedDate>
+    <PublishedDate>{format(publishedOn, 'MMM Do, y')}</PublishedDate>
   </MetaContainer>
 )
 
@@ -21,7 +23,7 @@ const PublishedDate: React.FC<{ children?: React.ReactNode }> = ({
 const MetaContainer: React.FC<{ children?: React.ReactNode }> = ({
   children
 }) => (
-  <div className="flex flex-col gap-3 rounded-md bg-gray-100 bg-opacity-60 p-5 text-on-surface-offBase shadow-lg backdrop-blur-xl backdrop-filter dark:bg-zinc-700 dark:bg-opacity-60 md:gap-10 md:p-16">
+  <div className="flex flex-col gap-3 rounded-md bg-gray-100 bg-opacity-60 p-5 text-on-surface-offBase shadow-lg backdrop-blur-xl backdrop-filter dark:bg-zinc-700 dark:bg-opacity-60 md:gap-8 md:p-14">
     {children}
   </div>
 )
