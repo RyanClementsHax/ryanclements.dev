b07ad6043bef1ba0a6413d3fdca0303a2de2946f
Ryan Clements <ryanclementshax@gmail.com>
2022-12-26 16:43:20 -0500

    remove mono font and style inline code

--- components/pages/posts/[slug]/Content/Content.tsx
+++ components/pages/posts/[slug]/Content/Content.tsx
@@ -1,6 +1,8 @@
 import { useReactFromHast } from 'lib/util/markdown/client'
 import { HastTree } from 'lib/util/markdown/types'
 import { ComponentsWithoutNodeOptions } from 'rehype-react/lib/complex-types'
+import s from './Content.module.scss'
+import c from 'classnames'
 
 const components: ComponentsWithoutNodeOptions['components'] = {
   // h2: H2
@@ -9,7 +11,9 @@ const components: ComponentsWithoutNodeOptions['components'] = {
 export const Content: React.FC<{ root: HastTree }> = ({ root }) => {
   const children = useReactFromHast(root, components)
   return (
-    <div className="prose prose-zinc max-w-none dark:prose-invert">
+    <div
+      className={c(s.content, 'prose prose-zinc max-w-none dark:prose-invert')}
+    >
       {children}
     </div>
   )
