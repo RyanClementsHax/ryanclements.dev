bc7f6d3ac1079888adbae574adff8cc55eb7f83c
Ryan Clements <ryanclementshax@gmail.com>
2022-12-27 11:18:24 -0500

    only build published pages for production

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,3 @@
 export const INITIAL_YEAR = 2022
+
+export const isDev = process.env.NODE_ENV === 'development'
--- lib/content/posts.ts
+++ lib/content/posts.ts
@@ -3,18 +3,9 @@ import path from 'path'
 import * as yup from 'yup'
 import { log } from 'lib/util'
 import { parseFrontMatter, validateMarkdown } from 'lib/util/markdown/server'
+import { isDev } from 'lib/constants'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
-
-export const getAllPostSlugs = async (): Promise<string[]> => {
-  try {
-    const files = await fs.readdir(postsDirectory).catch(() => [] as string[])
-    return files.map(x => x.replace('.md', ''))
-  } catch (e) {
-    log.error(`Was not able to read ${postsDirectory}`, e)
-    throw e
-  }
-}
 
 export interface Post {
   meta: PostMeta
@@ -22,16 +13,34 @@ export interface Post {
 }
 
 export interface PostMeta {
+  slug: string
   title: string
   publishedOn?: Date
   bannerSrc: string
+}
+
+export const getAllPostSlugs = async (): Promise<string[]> =>
+  (await getAllPosts())
+    .filter(x => isDev || !!x.meta.publishedOn)
+    .map(x => x.meta.slug)
+
+export const getAllPosts = async (): Promise<Post[]> => {
+  try {
+    const files = await fs.readdir(postsDirectory)
+    return await Promise.all(
+      files.map(x => x.replace('.md', '')).map(x => getPost(x))
+    )
+  } catch (e) {
+    log.error(`Was not able to read ${postsDirectory}`, e)
+    throw e
+  }
 }
 
 export const getPost = async (slug: string): Promise<Post> => {
   try {
     const rawPostString = await getRawPostString(slug)
     await validateMarkdown(rawPostString)
-    return await convertRawStringToPost(rawPostString)
+    return await convertRawStringToPost(slug, rawPostString)
   } catch (e: unknown) {
     log.error(`Could not get post for ${slug}`, e)
     throw e
@@ -41,8 +50,14 @@ export const getPost = async (slug: string): Promise<Post> => {
 const getRawPostString = async (slug: string): Promise<string> =>
   await fs.readFile(path.join(postsDirectory, `${slug}.md`), 'utf-8')
 
-const convertRawStringToPost = async (rawString: string): Promise<Post> => ({
-  meta: await getMetaFromRawString(rawString),
+const convertRawStringToPost = async (
+  slug: string,
+  rawString: string
+): Promise<Post> => ({
+  meta: {
+    slug,
+    ...(await getMetaFromRawString(rawString))
+  },
   content: rawString
 })
 
@@ -52,7 +67,7 @@ const postMetaSchema = yup.object({
   bannerSrc: yup.string().required()
 })
 
-const getMetaFromRawString = async (rawString: string): Promise<PostMeta> => {
+const getMetaFromRawString = async (rawString: string) => {
   const frontMatter = parseFrontMatter(rawString)
   return await postMetaSchema.validate(frontMatter)
 }
