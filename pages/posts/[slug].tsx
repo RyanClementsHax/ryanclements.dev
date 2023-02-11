451cb257879a335e42a276eaee3492586801f096
Ryan Clements <ryanclementshax@gmail.com>
2023-02-11 17:12:44 -0500

    print env in build

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -56,7 +56,7 @@ const PostPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
           },
           images: [
             {
-              url: 'https://res.cloudinary.com/kentcdodds-com/image/upload/$th_1256,$tw_2400,$gw_$tw_div_24,$gh_$th_div_12/co_rgb:a9adc1,c_fit,g_north_west,w_$gw_mul_14,h_$gh,x_$gw_mul_1.5,y_$gh_mul_1.3,l_text:kentcdodds.com:Matter-Regular.woff2_50:Check%2520out%2520this%2520article/co_white,c_fit,g_north_west,w_$gw_mul_13.5,h_$gh_mul_7,x_$gw_mul_1.5,y_$gh_mul_2.3,l_text:kentcdodds.com:Matter-Regular.woff2_110:Get%2520a%2520catch%2520block%2520error%2520message%2520with%2520TypeScript/c_fit,g_north_west,r_max,w_$gw_mul_4,h_$gh_mul_3,x_$gw,y_$gh_mul_8,l_kent:profile-transparent/co_rgb:a9adc1,c_fit,g_north_west,w_$gw_mul_5.5,h_$gh_mul_4,x_$gw_mul_4.5,y_$gh_mul_9,l_text:kentcdodds.com:Matter-Regular.woff2_70:Kent%20C.%20Dodds/co_rgb:a9adc1,c_fit,g_north_west,w_$gw_mul_9,x_$gw_mul_4.5,y_$gh_mul_9.8,l_text:kentcdodds.com:Matter-Regular.woff2_40:kentcdodds.com%252Fblog%252Fget-a-catch-block-error-message-with-typescript/c_fill,ar_3:4,r_12,g_east,h_$gh_mul_10,x_$gw,l_unsplash:photo-1525785967371-87ba44b3e6cf/c_fill,w_$tw,h_$th/kentcdodds.com/social-background.png',
+              url: `${SITE_URL}${post.meta.bannerSrc.src}`,
               width: post.meta.bannerSrc.width,
               height: post.meta.bannerSrc.height,
               alt: post.meta.bannerSrc.alt
