e913c2755e807a9e3da725f3ced518342a50a781
Ryan Clements <ryanclementshax@gmail.com>
2023-02-11 17:39:04 -0500

    dont error if NEXT_PUBLIC_URL cannot be found

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,11 +4,7 @@ export const IS_DEV = process.env.NODE_ENV === 'development'
 export const IS_PREVIEW = process.env.VERCEL_ENV === 'preview'
 
 export const SITE_URL: string =
-  'https://' +
-  (process.env.NEXT_PUBLIC_URL ||
-    (() => {
-      throw new Error('Did you forget to set NEXT_PUBLIC_URL?')
-    })())
+  'https://' + (process.env.NEXT_PUBLIC_URL ?? 'localhost:3000')
 
 export const RSS_FEED_URL = `${SITE_URL}/rss/feed.xml`
 export const JSON_FEED_URL = `${SITE_URL}/rss/feed.json`
