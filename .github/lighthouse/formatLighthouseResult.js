f22022b0a234cec0f660c7af6c7438309563de71
Ryan Clements <ryanclementshax@gmail.com>
2023-02-23 09:09:40 -0500

    fix lighthouse result comment formatting and add design system to lighthouse report

--- .github/lighthouse/formatLighthouseResult.js
+++ .github/lighthouse/formatLighthouseResult.js
@@ -1,7 +1,6 @@
-module.exports = ({ title, links, manifest, core }) => {
+module.exports = ({ type, links, manifest, core }) => {
   const runs = [
     {
-      title,
       links,
       results: manifest.filter(result => result.isRepresentativeRun)
     }
@@ -9,32 +8,32 @@ module.exports = ({ title, links, manifest, core }) => {
 
   const gradeScore = res => (res >= 90 ? '🟢' : res >= 50 ? '🟠' : '🔴')
 
-  const formatRun = ({ title, links, results }) =>
+  const formatRun = ({ links, results }) =>
     results
       .map(result => {
         const summary = result.summary
         const url = new URL(result.url)
 
         return `
-    ## ${title} ${url.pathname}
-    🌎 [${url}](${url})
-    ⚡️ [Report](${links[url.toString()]})
+## ${url.pathname}
+🌎 [${url}](${url})
+⚡️ [Report](${links[url.toString()]})
 
-    | Category | Score |
-    | --- | --- |
-    ${Object.keys(summary)
-      .map(key => {
-        const percentage = Math.round(summary[key] * 100)
-        return `| ${gradeScore(percentage)} ${key} | ${percentage} |`
-      })
-      .join('\n')}
+| Category | Score |
+| --- | --- |
+${Object.keys(summary)
+  .map(key => {
+    const percentage = Math.round(summary[key] * 100)
+    return `| ${gradeScore(percentage)} ${key} | ${percentage} |`
+  })
+  .join('\n')}
     `
       })
       .join('---')
 
   const comment = `
-    # Lighthouse Results
-    ${runs.map(formatRun).join('')}
+# ${type} Lighthouse Results
+${runs.map(formatRun).join('')}
     `
 
   core.setOutput('comment', comment)
