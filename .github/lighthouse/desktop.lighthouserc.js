6c8241a25b10542acf87ea3076839e7b796c2104
Ryan Clements <ryanclementshax@gmail.com>
2023-02-22 20:51:27 -0500

    turn off annoying/unneeded assertions

--- .github/lighthouse/desktop.lighthouserc.js
+++ .github/lighthouse/desktop.lighthouserc.js
@@ -40,7 +40,29 @@ module.exports = {
     },
     assert: {
       // https://github.com/GoogleChrome/lighthouse-ci/blob/d7240dcb25ff67aa74cce5067170a5f1a7a446a2/docs/configuration.md#assert
-      preset: 'lighthouse:no-pwa'
+      preset: 'lighthouse:no-pwa',
+      assertMatrix: [
+        {
+          matchingUrlPattern: '.*',
+          assertions: {
+            'csp-xss': 'off',
+            'uses-rel-preconnect': 'off',
+            'uses-responsive-images': 'off',
+            'efficient-animated-content': 'off'
+          }
+        },
+        {
+          matchingUrlPattern: 'https://[^/]+/posts/.+',
+          assertions: {
+            // Code block non-highlighted line opacity seems to trip this up
+            'color-contrast': 'off',
+            // Right now, the AST downloaded on a post is really large
+            // I hope to fix this later with server components
+            'total-byte-weight': 'off',
+            'link-text': 'off'
+          }
+        }
+      ]
     }
   }
 }
