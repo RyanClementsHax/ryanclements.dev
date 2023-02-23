39826d14043c9c6f235b924b4158d5791527a5d7
Ryan Clements <ryanclementshax@gmail.com>
2023-02-22 21:27:27 -0500

    add back in matrix and add config for about page

--- .github/lighthouse/mobile.lighthouserc.js
+++ .github/lighthouse/mobile.lighthouserc.js
@@ -1,4 +1,12 @@
 // https://github.com/GoogleChrome/lighthouse-ci
+
+const defaultAssertionRules = {
+  'csp-xss': 'off',
+  'uses-rel-preconnect': 'off',
+  'uses-responsive-images': 'off',
+  'efficient-animated-content': 'off'
+}
+
 module.exports = {
   ci: {
     // https://github.com/GoogleChrome/lighthouse/blob/main/docs/configuration.md#lighthouse-configuration
@@ -17,21 +25,37 @@ module.exports = {
       }
     },
     assert: {
-      preset: 'lighthouse:no-pwa',
-      assertions: {
-        // There isn't a good way to avoid duplication right now
-        // https://github.com/GoogleChrome/lighthouse-ci/issues/511
-        'csp-xss': 'off',
-        'uses-rel-preconnect': 'off',
-        'uses-responsive-images': 'off',
-        'efficient-animated-content': 'off',
-        // Code block non-highlighted line opacity seems to trip this up
-        'color-contrast': 'off',
-        // Right now, the AST downloaded on a post is really large
-        // I hope to fix this later with server components
-        'total-byte-weight': 'off',
-        'link-text': 'off'
-      }
+      assertMatrix: [
+        {
+          matchingUrlPattern: 'https://[^/]+/(?!posts/.+).*',
+          // https://github.com/GoogleChrome/lighthouse-ci/blob/d7240dcb25ff67aa74cce5067170a5f1a7a446a2/docs/configuration.md#assert
+          preset: 'lighthouse:no-pwa',
+          assertions: defaultAssertionRules
+        },
+        {
+          matchingUrlPattern: 'posts/.+',
+          // https://github.com/GoogleChrome/lighthouse-ci/blob/d7240dcb25ff67aa74cce5067170a5f1a7a446a2/docs/configuration.md#assert
+          preset: 'lighthouse:no-pwa',
+          assertions: {
+            ...defaultAssertionRules,
+            // Code block non-highlighted line opacity seems to trip this up
+            'color-contrast': 'off',
+            // Right now, the AST downloaded on a post is really large
+            // I hope to fix this later with server components
+            'total-byte-weight': 'off',
+            'link-text': 'off'
+          }
+        },
+        {
+          matchingUrlPattern: '/about',
+          // https://github.com/GoogleChrome/lighthouse-ci/blob/d7240dcb25ff67aa74cce5067170a5f1a7a446a2/docs/configuration.md#assert
+          preset: 'lighthouse:no-pwa',
+          assertions: {
+            ...defaultAssertionRules,
+            'offscreen-images': 'off'
+          }
+        }
+      ]
     }
   }
 }
