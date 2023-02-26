2b10c6d74c0f7737d974933ef87d996b71112688
Ryan Clements <ryanclementshax@gmail.com>
2023-02-26 10:31:35 -0500

    fix default metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,31 @@
 import { ConsoleMessage } from 'components/ConsoleMessage'
+import { themeMetadata } from 'components/theme'
 import { ThemeProvider } from 'components/theme/ThemeContext'
 import { ThemeScript } from 'components/theme/ThemeScript'
+import { SITE_URL } from 'lib/constants'
+import { rssMetadata } from 'lib/content/rss'
 import { fontClass } from 'lib/fonts'
 
 import 'styles/global.scss'
+
+export const metadata = {
+  title: 'Ryan Clements',
+  description:
+    'Full Time Catholic | Full Time Father | Full Stack Engineer | Massive Nerd',
+  icons: {
+    icon: '/favicon.ico'
+  },
+  openGraph: {
+    url: `${SITE_URL}/`
+  },
+  twitter: {
+    site: '@RyanClementsHax',
+    creator: '@RyanClementsHax',
+    card: 'summary_large_image'
+  },
+  ...themeMetadata,
+  ...rssMetadata
+}
 
 export default function RootLayout({
   children
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,7 @@
 import { Home } from 'components/pages/home'
-import { themeMetadata } from 'components/theme'
-import { IS_DEV, SITE_URL } from 'lib/constants'
-import { generateRssFeed, rssMetadata } from 'lib/content/rss'
+import { IS_DEV } from 'lib/constants'
+import { generateRssFeed } from 'lib/content/rss'
 import { getRenderablePostSummaries } from 'lib/pages/posts'
-
-export const metadata = {
-  title: 'Ryan Clements',
-  description:
-    'Full Time Catholic | Full Time Father | Full Stack Engineer | Massive Nerd',
-  icons: {
-    icon: '/favicon.ico'
-  },
-  openGraph: {
-    url: `${SITE_URL}/`
-  },
-  twitter: {
-    site: '@RyanClementsHax',
-    creator: '@RyanClementsHax',
-    card: 'summary_large_image'
-  },
-  ...themeMetadata,
-  ...rssMetadata
-}
 
 export default async function HomePage(): Promise<JSX.Element> {
   if (!IS_DEV) {
