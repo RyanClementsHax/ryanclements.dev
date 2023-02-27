42587b31ffa4693b01f0626e5c6ed8f65699cec5
Ryan Clements <ryanclementshax@gmail.com>
2023-02-27 10:23:39 -0500

    reenable rules, move efficient-animated-content to post page, and add comments for all disabled assertions

--- .github/lighthouse/assertConfig.js
+++ .github/lighthouse/assertConfig.js
@@ -1,29 +1,37 @@
 const defaultAssertionRules = {
-  // 'csp-xss': 'off',
-  // 'uses-rel-preconnect': 'off',
-  // 'uses-responsive-images': 'off',
-  // 'efficient-animated-content': 'off',
-  // // vercel's live feedback widget interferes with coverage metrics
-  // // can double check this with chrome's coverage tool
-  // 'unused-javascript': 'off',
-  // // something about next.js or vercel makes the rss feed link prefetched which
-  // // for some reason 404's
-  // 'errors-in-console': 'off'
+  // This is annoying to fix and unnecessary to fix from what I gather
+  // given the nature of this website
+  'csp-xss': 'off',
+  // This is caused by vercel's live feedback widget
+  'uses-rel-preconnect': 'off',
+  // Some of the images I use is cut off based off of css I apply to it like post banners
+  'uses-responsive-images': 'off',
+  // vercel's live feedback widget interferes with coverage metrics
+  // can double check this with chrome's coverage tool
+  'unused-javascript': 'off',
+  // something about next.js or vercel makes the rss feed link prefetched which
+  // for some reason 404's
+  'errors-in-console': 'off'
 }
 
 const pathToOverrides = {
   'posts/.+': {
-    // // Code block non-highlighted line opacity seems to trip this up
-    // 'color-contrast': 'off',
-    // // Right now, the AST downloaded on a post is really large
-    // // I hope to fix this later with server components
-    // 'total-byte-weight': 'off',
-    // // Checkboxes seem to fail this, but they're there for style only
-    // // Github seems to style checkboxes in comments in the same way
-    // label: 'off'
+    // Code block non-highlighted line opacity seems to trip this up
+    'color-contrast': 'off',
+    // This is caused by gifs being really large
+    // https://developer.chrome.com/docs/lighthouse/performance/efficient-animated-content/
+    'total-byte-weight': 'off',
+    // This is caused by gifs being really large
+    // https://developer.chrome.com/docs/lighthouse/performance/efficient-animated-content/
+    'efficient-animated-content': 'off',
+    // Checkboxes seem to fail this, but they're there for style only
+    // Github seems to style checkboxes in comments in the same way
+    label: 'off'
   },
   about: {
-    // 'offscreen-images': 'off'
+    // This is caused by the hero being rendered even though it isn't seen on mobile
+    // Not sure how to fix this one
+    'offscreen-images': 'off'
   }
 }
 
