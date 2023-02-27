a5e3ba095655479bb99693c708d9c5933d40056b
Ryan Clements <ryanclementshax@gmail.com>
2023-02-27 09:23:13 -0500

    deep merge root metadata to avoid accidental overwrites

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,27 +5,31 @@ import { ThemeScript } from 'components/theme/ThemeScript'
 import { SITE_URL } from 'lib/constants'
 import { rssMetadata } from 'lib/content/rss'
 import { fontClass } from 'lib/fonts'
+import deepmerge from '@fastify/deepmerge'
 
 import 'styles/global.scss'
+import { Metadata } from 'next'
 
-export const metadata = {
-  title: 'Ryan Clements',
-  description:
-    'Full Time Catholic | Full Time Father | Full Stack Engineer | Massive Nerd',
-  icons: {
-    icon: '/favicon.ico'
+export const metadata = deepmerge({ all: true })<Metadata[]>(
+  {
+    title: 'Ryan Clements',
+    description:
+      'Full Time Catholic | Full Time Father | Full Stack Engineer | Massive Nerd',
+    icons: {
+      icon: '/favicon.ico'
+    },
+    openGraph: {
+      url: `${SITE_URL}/`
+    },
+    twitter: {
+      site: '@RyanClementsHax',
+      creator: '@RyanClementsHax',
+      card: 'summary_large_image'
+    }
   },
-  openGraph: {
-    url: `${SITE_URL}/`
-  },
-  twitter: {
-    site: '@RyanClementsHax',
-    creator: '@RyanClementsHax',
-    card: 'summary_large_image'
-  },
-  ...themeMetadata,
-  ...rssMetadata
-}
+  themeMetadata,
+  rssMetadata
+)
 
 export default function RootLayout({
   children
