f931504c4269dc2c8737be57143a1be5a8896399
Ryan Clements <ryanclementshax@gmail.com>
2023-03-06 21:01:51 -0500

    prevent committing gifs

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -7,6 +7,10 @@ module.exports = {
     'yarn format'
   ],
   'posts/**/*.md': ['yarn post:mark-updated', 'yarn format'],
+  'public/posts/**/*.gif': [
+    filenames => `echo "Do not commit gif files\n\t${filenames.join('\n\t')}"`,
+    'exit 1'
+  ],
   'public/posts/**/banner.@(jpg|png)': ['yarn post:validate-banner'],
   '*.json|*.*rc!.browserslistrc': ['yarn format'],
   '*.@(css|scss)': ['yarn lint:styles', 'yarn format']
