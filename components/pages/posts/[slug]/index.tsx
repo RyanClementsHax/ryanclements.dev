34a603b2af01979045399c201ff53c9136b93ec4
Ryan Clements <ryanclementshax@gmail.com>
2023-03-09 10:08:53 -0500

    add buy me a coffee button to bottom of posts

--- components/pages/posts/[slug]/index.tsx
+++ components/pages/posts/[slug]/index.tsx
@@ -4,6 +4,7 @@ import { MetaCard } from './MetaCard'
 import { Content } from './Content'
 import { Layout } from 'components/Layout'
 import { A11yStaticImageData } from 'lib/content/images'
+import Link from 'next/link'
 
 export interface PostDetailsProps {
   post: RenderablePost
@@ -21,6 +22,7 @@ export const PostDetails: React.FC<PostDetailsProps> = ({
         updatedAt={meta.updatedAt}
       />
       <Content root={content} />
+      <Closer />
     </ContentContainer>
   </Layout>
 )
@@ -41,7 +43,31 @@ const Banner: React.FC<{ src: A11yStaticImageData }> = ({
 const ContentContainer: React.FC<{ children?: React.ReactNode }> = ({
   children
 }) => (
-  <article className="mx-auto -mt-8 mb-16 flex max-w-2xl flex-col gap-10 px-5 md:mb-16 md:-mt-16 md:gap-16">
+  <article className="mx-auto -mt-8 mb-16 flex max-w-2xl flex-col gap-12 px-5 md:mb-16 md:-mt-16 md:gap-16">
     {children}
   </article>
+)
+
+const Closer: React.FC = () => (
+  <section className="flex flex-col gap-12">
+    <hr className="border-zinc-200 dark:border-zinc-700" />
+    <p className="text-center">
+      Did you enjoy the post? Consider supporting me and my tea addition 🤗🍵.
+    </p>
+    <BuyMeACoffeeButton />
+  </section>
+)
+
+const BuyMeACoffeeButton: React.FC = () => (
+  <Link
+    href="https://www.buymeacoffee.com/ryanclementshax"
+    className="relative block h-12"
+  >
+    <Image
+      src="https://img.buymeacoffee.com/button-api/?text=Buy me a tea&emoji=🍵&slug=ryanclementshax&button_colour=FFDD00&font_colour=000000&font_family=Lato&outline_colour=000000&coffee_colour=ffffff"
+      alt="Buy me a coffee"
+      fill
+      unoptimized
+    />
+  </Link>
 )
